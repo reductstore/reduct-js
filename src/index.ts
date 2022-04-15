9274232515ae329d4f4348200056d09e3c105a7b
Aleksey Timin <atimin@gmail.com>
2022-04-15 22:58:56 +0200

    add quota type to index

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {Client} from "./Client";
 import {Bucket} from "./Bucket";
 import {APIError} from "./APIError";
 import {ServerInfo} from "./ServerInfo";
-import {BucketSettings} from "./BucketSettings";
+import {BucketSettings, QuotaType} from "./BucketSettings";
 import {BucketInfo} from "./BucketInfo";
 import {EntryInfo} from "./EntryInfo";
 
@@ -12,6 +12,7 @@ export {
     APIError,
     ServerInfo,
     BucketSettings,
+    QuotaType,
     BucketInfo,
     EntryInfo
 };
