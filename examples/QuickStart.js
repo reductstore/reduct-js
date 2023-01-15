68ccb8836a486a12bf484a709dffd78ac38769b8
Aleksey Timin <atimin@gmail.com>
2023-01-15 22:53:09 +0100

    finixh quick start

--- examples/QuickStart.js
+++ examples/QuickStart.js
@@ -7,26 +7,34 @@ const main = async () => {
     const client = new Client("http://127.0.0.1:8383");
     // --8<-- [end:createclient]
 
-    // Get or create a bucket with 1Gb quotan
+    // --8<-- [start:createbucket]
+    // Get or create a bucket with 1Gb quota
     const bucket = await client.getOrCreateBucket("my-bucket",
         {quotaType: QuotaType.FIFO, quotaSize: BigInt(1e9)}
     );
+    // --8<-- [end:createbucket]
 
+    // --8<-- [start:writedata]
     // The simplest case. Write some data with the current timestamp
     let record = await bucket.beginWrite("entry");
     await record.write("Hello, world!");
+    // --8<-- [end:writedata]
 
+    // --8<-- [start:uploadfile]
     // More complex case. Upload a file as a stream with a custom timestamp unix timestamp in microseconds
     const timestamp = Date.now() * 1000;
     record = await bucket.beginWrite("entry", timestamp);
     const fileStream = await fs.createReadStream(__filename);   // let's upload this file
     await record.write(fileStream, fs.statSync(__filename).size);
+    // --8<-- [end:uploadfile]
 
-    // The simplest case. Read the data by a certain ts
+    // --8<-- [start:readdata]
+    // The simplest case. Read the data by a certain timestamp
     record = await bucket.beginRead("entry", timestamp);
     console.log(await record.readAsString());
+    // --8<-- [end:readdata]
 
-    
+    // --8<-- [start:query]
     // More complex case. Iterate over all records in the entry and read them as steams
     function streamToString (stream) {
         const chunks = [];
@@ -36,7 +44,7 @@ const main = async () => {
             stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
         });
     }
-    
+
     for await (const record of bucket.query("entry")) {
         console.log(`Record timestamp: ${record.timestamp}`);
         console.log(`Record size: ${record.size}`);
@@ -44,6 +52,7 @@ const main = async () => {
         // Read the record as a stream
         await streamToString(record.stream);
     }
+    // --8<-- [end:query]
 
 };
 
