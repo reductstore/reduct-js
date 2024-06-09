89aa8c9cc46c9c5df7c5e8794b4d8b9376900ced
Alexey Timin <atimin@gmail.com>
2024-06-09 09:43:11 +0200

    remove documentaition and update README

--- examples/QuickStart.js
+++ examples/QuickStart.js
@@ -1,59 +1,30 @@
 const { Client, QuotaType } = require("../lib/cjs/index.js");
-const fs = require("fs");
 
-const main = async () => {
-  // --8<-- [start:createclient]
-  // Create a client
-  const client = new Client("http://127.0.0.1:8383");
-  // --8<-- [end:createclient]
+// 1. Create a ReductStore client
+const client = new Client("http://127.0.0.1:8383", {
+  apiToken: "my-token",
+});
 
-  // --8<-- [start:createbucket]
-  // Get or create a bucket with 1Gb quota
-  const bucket = await client.getOrCreateBucket("my-bucket", {
-    quotaType: QuotaType.FIFO,
-    quotaSize: BigInt(1e9),
-  });
-  // --8<-- [end:createbucket]
+// 2. Get or create a bucket with 1Gb quota
+const bucket = await client.getOrCreateBucket("my-bucket", {
+  quotaType: QuotaType.FIFO,
+  quotaSize: BigInt(1e9),
+});
 
-  // --8<-- [start:writedata]
-  // The simplest case. Write some data with the current timestamp
-  let record = await bucket.beginWrite("entry");
-  await record.write("Hello, world!");
-  // --8<-- [end:writedata]
+// 3. Write some data with timestamps in the 'sensor-1' entry
+const us = (dateString) => BigInt(Date.parse(dateString) * 1000);
+let record = await bucket.beginWrite("sensor-1", us("2021-01-01T00:00:00Z"));
+await record.write("Record #1");
+record = await bucket.beginWrite("sensor-1", us("2021-01-01T00:00:01Z"));
+await record.write("Record #2");
 
-  // --8<-- [start:uploadfile]
-  // More complex case. Upload a file as a stream with a custom timestamp unix timestamp in microseconds
-  const timestamp = Date.now() * 1000;
-  record = await bucket.beginWrite("entry", BigInt.valueOf(timestamp));
-  const fileStream = await fs.createReadStream(__filename); // let's upload this file
-  await record.write(fileStream, fs.statSync(__filename).size);
-  // --8<-- [end:uploadfile]
-
-  // --8<-- [start:readdata]
-  // The simplest case. Read the data by a certain timestamp
-  record = await bucket.beginRead("entry", timestamp);
+// 4. Query the data by time range
+for await (const record of bucket.query(
+  "sensor-1",
+  us("2021-01-01T00:00:00Z"),
+  us("2021-01-01T00:00:02Z"),
+)) {
+  console.log(`Record timestamp: ${record.timestamp}`);
+  console.log(`Record size: ${record.size}`);
   console.log(await record.readAsString());
-  // --8<-- [end:readdata]
-
-  // --8<-- [start:query]
-  // More complex case. Iterate over all records in the entry and read them as streams
-  function streamToString(stream) {
-    const chunks = [];
-    return new Promise((resolve, reject) => {
-      stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
-      stream.on("error", (err) => reject(err));
-      stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-    });
-  }
-
-  for await (const record of bucket.query("entry")) {
-    console.log(`Record timestamp: ${record.timestamp}`);
-    console.log(`Record size: ${record.size}`);
-
-    // Read the record as a stream
-    await streamToString(record.stream);
-  }
-  // --8<-- [end:query]
-};
-
-main().then(() => console.log("done"));
+}
