186831d5e0209ad40906eed9aba3e6042a8c6832
Anthony <26444489+AnthonyCvn@users.noreply.github.com>
2026-04-07 10:37:20 +0200

    fix: use relative assertion (#158)

--- test/Client.test.ts
+++ test/Client.test.ts
@@ -199,6 +199,8 @@ describe("Client", () => {
   });
 
   it("should get information about the server", async () => {
+    const baseInfo: ServerInfo = await client.getInfo();
+
     await client.createBucket("bucket_1");
     const bucket = await client.createBucket("bucket_2");
     let rec = await bucket.beginWrite("entry", 1000_000n);
@@ -210,11 +212,11 @@ describe("Client", () => {
     const info: ServerInfo = await client.getInfo();
     expect(info.version >= "1.10.0").toBeTruthy();
 
-    expect(info.bucketCount).toEqual(2n);
-    expect(info.usage).toEqual(82n);
+    expect(info.bucketCount).toEqual(baseInfo.bucketCount + 2n);
+    expect(info.usage).toEqual(baseInfo.usage + 82n);
     expect(info.uptime).toBeGreaterThanOrEqual(0);
-    expect(info.oldestRecord).toEqual(1000_000n);
-    expect(info.latestRecord).toEqual(2000_000n);
+    expect(info.oldestRecord).toBeLessThanOrEqual(1000_000n);
+    expect(info.latestRecord).toBeGreaterThanOrEqual(2000_000n);
 
     expect(info.defaults.bucket).toMatchObject({
       quotaSize: 0n,
